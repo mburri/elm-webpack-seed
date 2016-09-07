8216a3105f7f2d7cf16dff2017e135d76f93f8c5
Max Burri <burri@puzzle.ch>
2016-09-07 11:58:53 +0200

    added hot reloading

--- src/Main.elm
+++ src/Main.elm
@@ -1,5 +1,24 @@
-import Html exposing (Html, text)
+import Html exposing (Html, button, div, text, h1)
+import Html.App as App
+import Html.Events exposing (onClick)
 
-main: Html msg
 main =
-    text "hello world!"
+  App.beginnerProgram { model = 0, view = view, update = update }
+
+type Msg = Increment | Decrement
+
+update msg model =
+  case msg of
+    Increment ->
+      model + 1
+
+    Decrement ->
+      model - 1
+
+view model =
+  div []
+    [ h1 [] [text "counters"]
+    , button [ onClick Decrement ] [ text "-" ]
+    , div [] [ text (toString model) ]
+    , button [ onClick Increment ] [ text "+" ]
+    ]
--- webpack.config.js
+++ webpack.config.js
@@ -22,7 +22,7 @@ module.exports = {
             {
                 test: /\.elm$/,
                 exclude: [/elm-stuff/, /node_modules/],
-                loader: 'elm-webpack'
+                loader: 'elm-hot!elm-webpack'
             }
         ]
     },
