5f2560faa86c63cf5e3b5ef1f3f9e40a73010cd0
Max Burri <burri@puzzle.ch>
2016-10-04 08:43:48 +0200

    migrated from App.beginnerProgram to App.program

--- src/Main.elm
+++ src/Main.elm
@@ -1,24 +1,68 @@
+module Main exposing (..)
+
 import Html exposing (Html, button, div, text, h1)
 import Html.App as App
 import Html.Events exposing (onClick)
 
-main =
-  App.beginnerProgram { model = 0, view = view, update = update }
 
-type Msg = Increment | Decrement
+main =
+    App.program
+        { init = init
+        , view = view
+        , update = update
+        , subscriptions = subscriptions
+        }
+
+
+
+-- MODEL
+
+
+type alias Model =
+    { count : Int
+    }
+
+
+type Msg
+    = Increment
+    | Decrement
+
+
+init : ( Model, Cmd Msg )
+init =
+    ( Model 0, Cmd.none )
+
+
+
+-- UPDATE
+
 
 update msg model =
-  case msg of
-    Increment ->
-      model + 1
+    case msg of
+        Increment ->
+            ( { model | count = model.count + 1 }, Cmd.none )
 
-    Decrement ->
-      model - 1
+        Decrement ->
+            ( { model | count = model.count - 1 }, Cmd.none )
+
+
+
+-- SUBSCRIPTIONS
+
+
+subscriptions : Model -> Sub Msg
+subscriptions model =
+    Sub.none
+
+
+
+-- VIEW
+
 
 view model =
-  div []
-    [ h1 [] [text "counters"]
-    , button [ onClick Decrement ] [ text "-" ]
-    , div [] [ text (toString model) ]
-    , button [ onClick Increment ] [ text "+" ]
-    ]
+    div []
+        [ h1 [] [ text "counters" ]
+        , button [ onClick Decrement ] [ text "-" ]
+        , div [] [ text (toString model.count) ]
+        , button [ onClick Increment ] [ text "+" ]
+        ]
